84ec3c02a71b992a392ba307814f2c9e33735848
Victor Lima <victorlima090@gmail.com>
2025-06-15 14:54:54 -0300

    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,22 @@ from langchain_core.messages import AIMessage
 import streamlit as st
 from langchain_core.messages import  trim_messages
 
+st.title("Chatbot com LangGraph e Gemini")
+
+x = st.select_slider("Nível de educação",options=["Educado","Neutro","Mal-Educado"],value="Neutro")
+
+st.write(f"Nível de educação:{x}")
+
+st.title("Simple chat")
+
+# Inicializa o estado do chat
+if "show_chat" not in st.session_state:
+    st.session_state.show_chat = False
+
+# Botão para mostrar o chat
+if st.button("Abrir Chat"):
+    st.session_state.show_chat = True
+
 # region config
 API_KEY="AIzaSyBCG93yixkk84XyiOmZUu1-kINLjpwvFl0"
 
@@ -32,12 +48,22 @@ trimmer = trim_messages(
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
 
+prompt_educado = "Você é um atendente muito educado, responda todas as perguntas da melhor forma possível."
+prompt_neutro = "Você é um atendente neutro, responda todas as perguntas de forma neutra."
+prompt_mal_educado = "Você é um atendente muito mal educado, responda todas as perguntas da pior forma possível."
+prompt_educacao = ""
+if x == "Educado":
+    prompt_educacao = prompt_educado
+elif x == "Neutro":
+    prompt_educacao = prompt_neutro
+elif x == "Mal-Educado":
+    prompt_educacao = prompt_mal_educado
 #TODO define aqui o nível de educação do modelo
 prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "Você é um atendente muito  mal educado, responda todas as perguntas da pior forma possível.",
+            prompt_educacao,
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
@@ -63,63 +89,39 @@ memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
 config = {"configurable": {"thread_id": uuid_str}}
 
-st.title("Hello World!")
 
-st.header("This is a header", divider=True)
-
-st.markdown("## This is a markdown header")
-
-x = st.select_slider("Nível de educação",options=["Educado","Neutro","Mal-Educado","MUITO Mal-Educado"],)
-
-st.write(f"Nice level is {x}")
-
-st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
+if st.session_state.show_chat:
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("Posso ti ajudar em algo?"):    
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    if prompt := st.chat_input("Posso ti ajudar em algo?"):    
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    input_mesages = [HumanMessage(prompt)]
-    response = ""
-    
+        input_mesages = [HumanMessage(prompt)]
+        
 
-    def stream_response():
-        for chunk, metadata in app.stream(
-            {"messages": input_mesages},
-            config,
-            stream_mode="messages",
-        ):
-            if isinstance(chunk, AIMessage):
-                yield chunk.content
+        def stream_response():
+            response = ""
 
-    with st.chat_message("assistant"):
-        st.write_stream(stream_response())
-    
+            for chunk, metadata in app.stream(
+                {"messages": input_mesages},
+                config,
+                stream_mode="messages",
+            ):
+                if isinstance(chunk, AIMessage):
+                    response += chunk.content
+                    yield chunk.content
+            st.session_state.messages.append({"role": "assistant", "content": response})
 
-
-# #TODO get user input
-# query= "Ola eu sou o Victor, me de um poema com 1000 caracteres sobre a vide um programador, em portugues"
-
-
-
-# input_mesages = [HumanMessage(query)]
-# # output = app.invoke({"messages": input_mesages}, config)
-
-# for chunk, metadata in app.stream(
-#     {"messages": input_mesages},
-#     config,
-#     stream_mode="messages",
-# ):
-#     if isinstance(chunk, AIMessage):  # Filter to just model responses
-#         print(chunk.content)
+        with st.chat_message("assistant"):
+            st.write_stream(stream_response())
 
